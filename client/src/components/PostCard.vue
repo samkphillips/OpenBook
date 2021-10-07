72a35bb0d970a49acde83f7bf27f4c059bf42dca
Sam <sam.k.phillips@gmail.com>
2021-10-07 15:13:33 -0400

    Can show and hide comments

--- client/src/components/PostCard.vue
+++ client/src/components/PostCard.vue
@@ -2,12 +2,23 @@
   <div class="post">
     <h6>Name: {{ post.name }} - Posted on: {{ post.created_at }}</h6>
     <p>{{ post.content }}</p>
+    <h5>{{ post.likes - post.dislikes }} | Likes: {{ post.likes }} | Dislikes: {{ post.dislikes }}</h5>
     <div v-if="post.comments">
-      <CommentCard
-        v-for="comment in post.comments"
-        :key="comment.id"
-        :comment="comment"
-      />
+      <div v-if="displayComments">
+        <CommentCard
+          v-for="comment in post.comments"
+          :key="comment.id"
+          :comment="comment"
+        />
+        <button @click="() => {displayComments = false}">
+          Hide Comments
+        </button>
+      </div>
+      <div v-else>
+        <button @click="() => {displayComments = true}">
+          Display {{ post.comments.length }} Comments
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +31,9 @@ export default {
   components: {
     CommentCard
   },
+  data: () => ({
+    displayComments: false
+  }),
   props: {
     post: Object
   }
