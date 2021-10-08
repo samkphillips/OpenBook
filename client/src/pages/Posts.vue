45d7a3faff61cff8537215a3471245ad3e1ec6cc
Sam <sam.k.phillips@gmail.com>
2021-10-07 22:29:32 -0400

    Tweaked get post resource and continued hooking things up

--- client/src/pages/Posts.vue
+++ client/src/pages/Posts.vue
@@ -4,7 +4,7 @@
     <div>
       <PostForm :postList="postList" />
     </div>
-    <div>
+    <div v-if="postList.length > 0">
       <h5>Display Posts</h5>
       <PostCard
         v-for="post in postList"
@@ -12,11 +12,13 @@
         :post="post"
       />
     </div>
+    <div v-else><h6>Loading...</h6></div>
   </div>
 </template>
 
 <script>
-import posts from '../testing/dummyPosts'
+// import posts from '../testing/dummyPosts'
+import { GetPosts } from '../services/posts'
 
 import PostCard from '../components/PostCard.vue'
 import PostForm from '../components/PostForm.vue'
@@ -28,7 +30,16 @@ export default {
     PostForm
   },
   data: () => ({
-    postList: posts
-  })
+    postList: []
+  }),
+  mounted() {
+    this.fetchPosts()
+  },
+  methods: {
+    async fetchPosts() {
+      const messages = await GetPosts()
+      this.postList = messages
+    }
+  }
 }
 </script>
--- resources/post.py
+++ resources/post.py
@@ -7,7 +7,11 @@ from models.post import Post
 
 class Posts(Resource):
     def get(self):
-        posts = Post.find_all()
+        data = Post.query.options(joinedload('comments')).all()
+        posts = [{**d.json()} for d in data]
+        for i in range(len(posts)):
+            comments = [c.json() for c in data[i].comments]
+            posts[i]["comments"] = comments
         return posts
 
     def post(self):
@@ -22,7 +26,7 @@ class PostDetail(Resource):
     def get(self, post_id):
         post = Post.query.options(
             joinedload('comments')).filter_by(id=post_id).first()
-        print(post.comments)
+        print(post)
         comments = [p.json() for p in post.comments]
         return {**post.json(), "comments": comments}
 
